0b1c4c415be98c3f4675f85a9fd403c583db9e98
Tommaso P <tommaso.plinio@gmail.com>
2021-05-07 18:45:30 +0200

    img logo

---
+++ resources/views/home.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>La Molisana</title>
+   <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+   <header>
+      <div class="container">
+         <div class="logo">
+            <img src="{{ asset ('images/marchio-sito-test.png') }}" alt="immagine-logo">
+         </div>
+      </div>
+      <div class="container">
+         <nav>
+            <ul>
+               <li class="active"><a href=""></a>Home</li>
+               <li><a href=""></a>Prodotti</li>
+               <li><a href=""></a>News</li>
+            </ul>
+         </nav>
+      </div>
+         
+      
+      
+    
+   </header>
+</body>
+</html>
