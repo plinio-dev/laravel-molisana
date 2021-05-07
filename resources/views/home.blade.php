f5da230405286e044ada159387558769d871c276
Tommaso P <tommaso.plinio@gmail.com>
2021-05-07 19:15:26 +0200

    menu classe active

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,10 @@
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>La Molisana</title>
+   <!-- google font -->
+   <link rel="preconnect" href="https://fonts.gstatic.com">
+   <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap" rel="stylesheet">    
+   <!-- foglio di stile -->
    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
@@ -17,9 +21,9 @@
       <div class="container">
          <nav>
             <ul>
-               <li class="active"><a href=""></a>Home</li>
-               <li><a href=""></a>Prodotti</li>
-               <li><a href=""></a>News</li>
+               <li><a class="menu-font active" href="">Home</a></li>
+               <li><a class="menu-font" href="">Prodotti</a></li>
+               <li><a class="menu-font" href="">News</a></li>
             </ul>
          </nav>
       </div>
